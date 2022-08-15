56db45ce82c7f5157aaea57a1628c46711535c6c
ahmed nagy <a.nagy@nasps.org.eg>
2022-08-15 16:44:54 +0200

    fix thread problem in AsteroidCache -> deletePreviousDayAsteroidData() function

--- starter/app/src/main/java/com/udacity/asteroidradar/common/data/cache/AsteroidCache.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/common/data/cache/AsteroidCache.kt
@@ -3,7 +3,9 @@ package com.udacity.asteroidradar.common.data.cache
 import com.udacity.asteroidradar.common.data.cache.daos.AsteroidDao
 import com.udacity.asteroidradar.common.data.cache.entities.CachedAsteroid
 import com.udacity.asteroidradar.common.utils.DateUtils.getNextSevenDaysFormattedDates
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AsteroidCache @Inject constructor(
@@ -22,8 +24,12 @@ class AsteroidCache @Inject constructor(
     override suspend fun deletePreviousDayAsteroidData() {
 
         val dates = getNextSevenDaysFormattedDates()
-        dates.forEach {
-            asteroidDao.deleteAsteroid(it)
+
+        withContext(Dispatchers.IO) {
+            dates.forEach {
+                asteroidDao.deleteAsteroid(it)
+            }
+
         }
     }
 }
